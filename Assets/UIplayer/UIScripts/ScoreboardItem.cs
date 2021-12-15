0717276955e3f3333dcb090491409fb9dfc69ea3
Knightling23 <49211912+Knightling23@users.noreply.github.com>
2021-12-15 17:05:45 -0500

    Add files via upload

---
+++ Assets/UIplayer/UIScripts/ScoreboardItem.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+//using Photon.RealTime;
+
+public class ScoreboardItem : MonoBehaviour
+{
+    public Text usernameText;
+    public Text ScoreText;
+
+    public void Initialized()
+    {
+       // usernameText.text = Player.Nickname; 
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
