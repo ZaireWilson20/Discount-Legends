8377fef41929fd8d1a5ece41f270a5beb1194e38
Knightling23 <49211912+Knightling23@users.noreply.github.com>
2021-12-15 13:37:52 -0500

    Add files via upload

---
+++ Assets/UIplayer/UIScripts/Timerscript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timerscript : MonoBehaviour
+{
+    public float Timer = 90f;
+    public Text Timertext;
+
+    // Start is called before the first frame update
+   
+    // Update is called once per frame
+    void Update()
+    {
+        if (Timer > 0)
+        {
+            Timer -= Time.deltaTime;
+
+        }
+        else
+        {
+            Timer = 0;
+
+        }
+        DisplayTime(Timer);
+
+
+
+      
+    }
+
+
+    void DisplayTime(float TimeDisplay)
+    {
+        if (TimeDisplay < 0)
+        {
+            TimeDisplay = 0;
+
+        }
+
+        float minutes = Mathf.FloorToInt(TimeDisplay/60);
+        float seconds = Mathf.FloorToInt(TimeDisplay % 60);
+
+        Timertext.text = string.Format("{0:00}:{1:00}",minutes,seconds); 
+
+    }
+}
