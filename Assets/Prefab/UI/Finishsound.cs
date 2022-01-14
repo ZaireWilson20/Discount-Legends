5a9b881444d3b6601422011e7f1148cea50f6036
Knightling23 <49211912+Knightling23@users.noreply.github.com>
2022-01-13 20:09:03 -0500

    Add files via upload

---
+++ Assets/Prefab/UI/Finishsound.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class Finishsound : MonoBehaviour
+{
+    // Start is called before the first frame update
+AudioSource Finish;
+
+    private void Start()
+    {
+        Finish = GetComponent<AudioSource>();
+        Finish.PlayDelayed(89.5f);
+
+
+    }
+}
